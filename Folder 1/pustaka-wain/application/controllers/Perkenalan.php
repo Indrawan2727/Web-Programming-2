49f1e3d14bfd17750041fec950202e8c11bf8457
Wahyu Indrawan <banut2701@gmail.com>
2020-10-19 14:14:12 +0700

    first commit

---
+++ Folder 1/pustaka-wain/application/controllers/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php 
+class Perkenalan extends CI_Controller{
+	public function index(){
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h3>Nama saya Wahyu Indrawan<br>
+			Saya tinggal di daerah Tegal<br>
+			Olahraga yang saya sukai adalah Basket<h3>";
+	}
+}
+
+
